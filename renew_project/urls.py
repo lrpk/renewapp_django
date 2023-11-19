df419bc631c57dc8238ce6d9687605cb106b900b
Pavlo Klymenko <lorrdpk@gmail.com>
2023-11-19 18:35:59 +0200

    django authentication

--- renew_project/urls.py
+++ renew_project/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", include("renew_app.urls")),
     path("store/", include("store.urls")),
     path("cart/", include("cart.urls")),
+    path("accounts/", include("accounts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
